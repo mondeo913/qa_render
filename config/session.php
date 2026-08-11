c6cc75d661b4ac780c86bd50ac5dd0f14d500076
evanpugaruiz-cloud <evanpugaruiz@gmail.com>
2026-08-11 16:14:57 -0600

    Extend SIGET QA session lifetime

--- config/session.php
+++ config/session.php
@@ -1,6 +1,6 @@
 <?php
 return [
- 'driver'=>env('SESSION_DRIVER','database'),'lifetime'=>(int)env('SESSION_LIFETIME',120),
+ 'driver'=>env('SESSION_DRIVER','database'),'lifetime'=>(int)env('SESSION_LIFETIME',1440),
  'expire_on_close'=>false,'encrypt'=>true,'files'=>storage_path('framework/sessions'),
  'connection'=>env('SESSION_CONNECTION'),'table'=>env('SESSION_TABLE','sessions'),
  'store'=>env('SESSION_STORE'),'lottery'=>[2,100],'cookie'=>env('SESSION_COOKIE','siget_session'),
