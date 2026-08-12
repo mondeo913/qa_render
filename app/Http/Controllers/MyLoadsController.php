79e5c9858100114de9d926a3baeb67ebc65419f7
evanpugaruiz-cloud <evanpugaruiz@gmail.com>
2026-08-12 17:27:52 -0600

    FEAT: clasificar Mis cargas por dependencia y filtros programados

--- app/Http/Controllers/MyLoadsController.php
+++ app/Http/Controllers/MyLoadsController.php
@@ -14,7 +14,7 @@ class MyLoadsController extends Controller
         $user = $request->user();
         $unitIds = $access->accessibleUnitIds($user);
 
-        $loads = $access->scopeLoads(
+        $baseQuery = $access->scopeLoads(
             ScheduledLoad::query()
                 ->with([
                     'agency',
@@ -29,11 +29,74 @@ class MyLoadsController extends Controller
                             'evidences.files',
                         ]);
                     },
-                ])
-                ->orderByDesc('effective_open_at'),
+                ]),
             $user
-        )->paginate(18)->withQueryString();
+        );
 
-        return view('cargas.mis-cargas', compact('loads'));
+        if ($request->filled('agency_id')) {
+            $baseQuery->where('contracting_agency_id', $request->integer('agency_id'));
+        }
+
+        if ($request->filled('template_id')) {
+            $baseQuery->where('template_id', $request->integer('template_id'));
+        }
+
+        if ($request->filled('month')) {
+            $month = $request->string('month')->toString();
+            if (preg_match('/^\d{4}-\d{2}$/', $month)) {
+                $baseQuery
+                    ->whereDate('effective_open_at', '>=', $month . '-01')
+                    ->whereDate('effective_open_at', '<', date('Y-m-d', strtotime($month . '-01 +1 month')));
+            }
+        }
+
+        if ($request->filled('unit_id')) {
+            $unitId = $request->integer('unit_id');
+            $baseQuery->whereHas('deliverables', function ($query) use ($unitId, $access, $user, $unitIds) {
+                if ($unitIds !== []) {
+                    $access->scopeDeliverables($query, $user);
+                }
+                $query->where('organizational_unit_id', $unitId);
+            });
+        }
+
+        $loads = (clone $baseQuery)
+            ->orderByDesc('effective_open_at')
+            ->paginate(18)
+            ->withQueryString();
+
+        $filterLoads = (clone $baseQuery)
+            ->without(['deliverables'])
+            ->with(['agency', 'template'])
+            ->orderByDesc('effective_open_at')
+            ->get();
+
+        $filterUnits = (clone $baseQuery)
+            ->without(['deliverables'])
+            ->with(['deliverables.organizationalUnit'])
+            ->get()
+            ->flatMap(fn ($load) => $load->deliverables->pluck('organizationalUnit'))
+            ->filter()
+            ->unique('id')
+            ->sortBy('name')
+            ->values();
+
+        $agencies = $filterLoads->pluck('agency')->filter()->unique('id')->sortBy('name')->values();
+        $templates = $filterLoads->pluck('template')->filter()->unique('id')->sortBy('name')->values();
+        $months = $filterLoads
+            ->pluck('effective_open_at')
+            ->filter()
+            ->map(fn ($date) => $date->format('Y-m'))
+            ->unique()
+            ->sortDesc()
+            ->values();
+
+        return view('cargas.mis-cargas', compact(
+            'loads',
+            'agencies',
+            'templates',
+            'months',
+            'filterUnits'
+        ));
     }
 }
