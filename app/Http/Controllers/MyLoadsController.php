3e6f8dd434e10d332c81fafb5ae947fe2feff09f
evanpugaruiz-cloud <evanpugaruiz@gmail.com>
2026-08-13 13:42:16 -0600

    FIX: limitar filtros de Transmisión y meses por pauta

--- app/Http/Controllers/MyLoadsController.php
+++ app/Http/Controllers/MyLoadsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\RoleCode;
 use App\Models\ScheduledLoad;
 use App\Services\AccessScopeService;
 use Illuminate\Contracts\View\View;
@@ -13,8 +14,11 @@ class MyLoadsController extends Controller
     {
         $user = $request->user();
         $unitIds = $access->accessibleUnitIds($user);
+        $operatorUnitId = RoleCode::isOperator($user->role?->code) && $user->organizational_unit_id
+            ? (int) $user->organizational_unit_id
+            : null;
 
-        $baseQuery = $access->scopeLoads(
+        $scopedQuery = $access->scopeLoads(
             ScheduledLoad::query()
                 ->with([
                     'agency',
@@ -34,8 +38,27 @@ class MyLoadsController extends Controller
         );
 
         if ($request->filled('agency_id')) {
-            $baseQuery->where('contracting_agency_id', $request->integer('agency_id'));
+            $scopedQuery->where('contracting_agency_id', $request->integer('agency_id'));
         }
+
+        if ($operatorUnitId !== null) {
+            // An operator is hard-scoped to the direction assigned to the account.
+            // A crafted unit_id must never expose another organizational unit.
+            $scopedQuery->whereHas('deliverables', function ($query) use ($access, $user, $operatorUnitId) {
+                $access->scopeDeliverables($query, $user);
+                $query->where('organizational_unit_id', $operatorUnitId);
+            });
+        } elseif ($request->filled('unit_id')) {
+            $unitId = $request->integer('unit_id');
+            $scopedQuery->whereHas('deliverables', function ($query) use ($unitId, $access, $user, $unitIds) {
+                if ($unitIds !== []) {
+                    $access->scopeDeliverables($query, $user);
+                }
+                $query->where('organizational_unit_id', $unitId);
+            });
+        }
+
+        $baseQuery = clone $scopedQuery;
 
         if ($request->filled('template_id')) {
             $baseQuery->where('template_id', $request->integer('template_id'));
@@ -50,28 +73,20 @@ class MyLoadsController extends Controller
             }
         }
 
-        if ($request->filled('unit_id')) {
-            $unitId = $request->integer('unit_id');
-            $baseQuery->whereHas('deliverables', function ($query) use ($unitId, $access, $user, $unitIds) {
-                if ($unitIds !== []) {
-                    $access->scopeDeliverables($query, $user);
-                }
-                $query->where('organizational_unit_id', $unitId);
-            });
-        }
-
         $loads = (clone $baseQuery)
             ->orderByDesc('effective_open_at')
             ->paginate(18)
             ->withQueryString();
 
-        $filterLoads = (clone $baseQuery)
+        // Filter options are scoped to the same user/direction, but do not apply
+        // the selected template/month so the month list can react to the template.
+        $filterLoads = (clone $scopedQuery)
             ->without(['deliverables'])
             ->with(['agency', 'template'])
             ->orderByDesc('effective_open_at')
             ->get();
 
-        $filterUnits = (clone $baseQuery)
+        $filterUnits = (clone $scopedQuery)
             ->without(['deliverables'])
             ->with(['deliverables.organizationalUnit'])
             ->get()
@@ -80,6 +95,10 @@ class MyLoadsController extends Controller
             ->unique('id')
             ->sortBy('name')
             ->values();
+
+        if ($operatorUnitId !== null) {
+            $filterUnits = $filterUnits->where('id', $operatorUnitId)->values();
+        }
 
         $agencies = $filterLoads->pluck('agency')->filter()->unique('id')->sortBy('name')->values();
         $templates = $filterLoads->pluck('template')->filter()->unique('id')->sortBy('name')->values();
@@ -91,12 +110,29 @@ class MyLoadsController extends Controller
             ->sortDesc()
             ->values();
 
+        $monthsByTemplate = $filterLoads
+            ->groupBy('template_id')
+            ->map(fn ($templateLoads) => $templateLoads
+                ->pluck('effective_open_at')
+                ->filter()
+                ->map(fn ($date) => $date->format('Y-m'))
+                ->unique()
+                ->sortDesc()
+                ->values()
+                ->all()
+            )
+            ->all();
+
+        $isDirectionLocked = $operatorUnitId !== null;
+
         return view('cargas.mis-cargas', compact(
             'loads',
             'agencies',
             'templates',
             'months',
-            'filterUnits'
+            'monthsByTemplate',
+            'filterUnits',
+            'isDirectionLocked'
         ));
     }
 }
